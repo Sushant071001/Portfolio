1bcc5d05edbc0a4db09e6cde5edc24f0e93d1957
Sushant071001 <ssp110266@gmail.com>
2023-07-21 15:08:16 +0530

    Portfolio Completed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+console.log("ddd");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY>200)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+function downloadCV() {
+    // Find the link element by its ID and click it programmatically
+    const downloadLink = document.getElementById("downloadLink");
+    downloadLink.click();
+}
